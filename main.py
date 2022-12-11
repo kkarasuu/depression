f0a885f1b97a188692d7329aa26893d030baf1b4
karasu <115457850+kkarasuu@users.noreply.github.com>
2022-12-11 21:10:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,28 +13,27 @@ data.dropna()
 
 # 1st simple plot - histogram
 fig = px.histogram(data, x="education_level")
-fig.show()
+st.plotly_chart(fig)
 
 # 2nd simple plot - scatter chart
 fig = px.scatter(data, x="living_expenses", y="Survey_id")
-fig.show()
+st.plotly_chart(fig)
 
 # There is a strange line, let's delete it
 data['living_expenses'] = pd.to_numeric(data['living_expenses'])
 data = data.loc[data['living_expenses'] != 26692283]
 
 fig = px.scatter(data, x="living_expenses", y="Survey_id")
-fig.show()
+st.plotly_chart(fig)
 
 # 3rd simple plot - pie chart
 fig = px.pie(data, names='total_members')
-fig.show()
+st.plotly_chart(fig)
 
 # 1st complex plot - bar chart
 fig = px.bar(data, x='total_members', y='depressed')
-fig.show()
+st.plotly_chart(fig)
 
 # 2nd complex plot - violin chart
 fig = px.violin(data, x="sex", y="Age", color="depressed")
-fig.show()
-
+st.plotly_chart(fig)
